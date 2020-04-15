4014a644c5be02321e74066bc494230bd3885945
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-04-15 13:56:18 +0100

    May of work 2?

--- main.py
+++ main.py
@@ -222,9 +222,9 @@ async def spotify(ctx, *, arg):
                 print('Obtained Spotify Token.')
                 spottoke = (js['access_token'])
                 print('Spottoke variable defined.')
-            async with session.post('https://api.spotify.com/v1/search?q={arg}&type=artists&limit=1', headers={'Authorization': 'Bearer '+ spottoke}) as r:
-                if r.status == 200:
-                    js = await r.json()
+            async with session.post('https://api.spotify.com/v1/search?q={arg}&type=artists&limit=1', headers={'Authorization': 'Bearer '+ spottoke}) as r1:
+                if r1.status == 200:
+                    js = await r1.json()
                     print('Printing JSON.')
                     await ctx.send(js['artists'])
 
