a9548a7b294a03ee2d03c094f1a65f5e89a2819b
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-04-16 00:50:11 +0100

    Unrigged the slot machine.

--- main.py
+++ main.py
@@ -104,14 +104,14 @@ async def slot(ctx, *, arg):
             number2 = random.randint(0,9)
             number3 = random.randint(0,9)
             await ctx.send('The display says '+str(number1)+' '+str(number2)+' '+str(number3)+'.')
-            if number1 == number3:
-                await ctx.send('The machine lights up! You\'ve won!\nThe machine spits out '+str(bet)+' Dosh')
+            if number1 == number2 and number2 == number3:
+                await ctx.send('The machine lights up and flashes! You\'ve won the jackpot!\nThe machine spits out '+str(bet*4)+' Dosh')
+                bank[str(ctx.message.author.id)] = money + bet*3
             elif number1 == number2 or number2 == number3:
                 await ctx.send('The machine lights up! You\'ve won!\nThe machine spits out '+str(bet*2)+' Dosh')
                 bank[str(ctx.message.author.id)] = money + bet
-            elif number1 == number2 and number2 == number3:
-                await ctx.send('The machine lights up and flashes! You\'ve won the jackpot!\nThe machine spits out '+str(bet*4)+' Dosh')
-                bank[str(ctx.message.author.id)] = money + bet*3
+            elif number1 == number3:
+                await ctx.send('The machine lights up! You\'ve won!\nThe machine spits out '+str(bet)+' Dosh')
             else:
                 await ctx.send('The machine plays a sad sound. You\'re in the hole '+str(bet)+' Dosh.')
                 bank[str(ctx.message.author.id)] = money - bet
@@ -228,7 +228,7 @@ async def spotify(ctx, *, arg):
         async with session.get('https://api.spotify.com/v1/search?q='+arg+'&type=artist&limit=1', headers={'Authorization': 'Bearer '+ spottoke}) as r1:
             if r1.status == 200:
                 js = await r1.json()
-                jsparse = json.loads(r1.json())
+                jsparse = await json.loads(r1.json())
                 await ctx.send(jsparse['artists']['items']['external_urls']['spotify'])
             else:
                 print(r1.status)
