a79c3a5c35f1b4ec2c523cd1134c3f444416aac3
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-04-15 14:35:16 +0100

    Indents, when?

--- main.py
+++ main.py
@@ -226,9 +226,9 @@ async def spotify(ctx, *, arg):
     async with aiohttp.ClientSession() as session:
         async with session.post('https://api.spotify.com/v1/search?q='+arg+'&type=artists&limit=1', headers={'Authorization': 'Bearer '+ spottoke}) as r1:
             if r1.status == 200:
-            js = await r1.json()
-            print('Printing JSON.')
-            await ctx.send(js['artists'])
+                js = await r1.json()
+                print('Printing JSON.')
+                await ctx.send(js['artists'])
 
 @bot.command(pass_context=True)
 async def say(ctx, *, arg):
