1c14f5ce237107c307cad81b965ab2a5d97e2a6e
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-04-15 11:18:58 +0100

    Had to do it to them. [Bad word filter populated]

--- brfilter.py
+++ brfilter.py
@@ -1,1 +1,1 @@
-badwords = ['fuck']
+badwords = ['fuck', 'shit', 'dick', 'asshole', 'bitch', 'bastard', 'cunt', 'bugger', 'choad', 'shag', 'wanker', 'wank', 'twat', 'nigga', 'nigger', 'niger', 'niga', 'n word', 'slut', 'whore', 'fag', 'faggot', 'cum', 'retard', 'blowjob', 'handjob', 'bhenchod', 'gandu', 'chutiya', 'saley', 'lund pe charo', 'sex', 'vagina', 'penis', 'pussy', 'tits', 'breasts', 'crap', 'porn', 'slag', 'spag', 'spastic', 'bollocks', 'motherfucker', 'arse', 'minger', 'bullshit', 'feck', 'fanny']
