b0772af69b756980bde86854e7736df225f5924f
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-04-16 11:38:46 +0100

    Plug the output to get the JSON object sent back from Spotify.

--- main.py
+++ main.py
@@ -229,7 +229,8 @@ async def spotify(ctx, *, arg):
             if r1.status == 200:
                 # Note to self: don't fuck with this code, you'll probably spend two days fixing it.
                 js = await r1.json()
-                jsfuckmeintheassplease = js{'artists'}{'items'}{'external_urls'}{'spotify'}
+                print(js)
+                jsfuckmeintheassplease = js['artists']['items']['external_urls']['spotify']
                 await ctx.send(jsfuckmeintheassplease)
             else:
                 print(r1.status)
