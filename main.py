33eceff45714ca09570c06f20d85cb970a4eb1b6
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-04-16 11:33:30 +0100

    AAAAAAAAAAAAAAAAAAAAAAAA

--- main.py
+++ main.py
@@ -229,8 +229,7 @@ async def spotify(ctx, *, arg):
             if r1.status == 200:
                 # Note to self: don't fuck with this code, you'll probably spend two days fixing it.
                 js = await r1.json()
-                jsparse = json.load(js)
-                jsfuckmeintheassplease = jsparse['artists']['items']['external_urls']['spotify']
+                jsfuckmeintheassplease = js['artists']['items']['external_urls']['spotify']
                 await ctx.send(jsfuckmeintheassplease)
             else:
                 print(r1.status)
