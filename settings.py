f81c74405719dffc4ef54392daeff084f3a0b8a7
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-05 22:08:45 +0100

    super indent

--- settings.py
+++ settings.py
@@ -16,26 +16,26 @@ class Settings(commands.Cog):
         self._last_member = None
 
     @commands.command(pass_context=True)
-		async def sayfilter(self, ctx):
-			"""[Settings] Toggle the filter for the say command."""
-			settings_filter = await main.bot_load_filter()
-			if ctx.message.author.id == ctx.message.guild.owner_id or ctx.message.author.id == config.owner:
-				if settings_filter.get(str(ctx.message.guild.id)) == None:
-					settings_filter[str(ctx.message.guild.id)] = 1
-					await ctx.send('Filtering disabled!')
-					await main.bot_save_filter(settings_filter)
+        async def sayfilter(self, ctx):
+            """[Settings] Toggle the filter for the say command."""
+            settings_filter = await main.bot_load_filter()
+            if ctx.message.author.id == ctx.message.guild.owner_id or ctx.message.author.id == config.owner:
+                if settings_filter.get(str(ctx.message.guild.id)) == None:
+                    settings_filter[str(ctx.message.guild.id)] = 1
+                    await ctx.send('Filtering disabled!')
+                    await main.bot_save_filter(settings_filter)
 
-				elif settings_filter[str(ctx.message.guild.id)] == 0:
-					settings_filter[str(ctx.message.guild.id)] = 1
-					await ctx.send('Filtering disabled!')
-					await main.bot_save_filter(settings_filter)
+                elif settings_filter[str(ctx.message.guild.id)] == 0:
+                    settings_filter[str(ctx.message.guild.id)] = 1
+                    await ctx.send('Filtering disabled!')
+                    await main.bot_save_filter(settings_filter)
 
-				elif settings_filter[str(ctx.message.guild.id)] == 1:
-					settings_filter[str(ctx.message.guild.id)] = 0
-					await ctx.send('Filtering enabled!')
-					await main.bot_save_filter(settings_filter)
-			else:
-				await ctx.send('Only server owners can set this!')
+                elif settings_filter[str(ctx.message.guild.id)] == 1:
+                    settings_filter[str(ctx.message.guild.id)] = 0
+                    await ctx.send('Filtering enabled!')
+                    await main.bot_save_filter(settings_filter)
+            else:
+                await ctx.send('Only server owners can set this!')
 
 def setup(bot):
     bot.add_cog(Settings(bot))
