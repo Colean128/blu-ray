ba703918de48c79dd8e6393b003cfa53ccf0fafd
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-06 12:42:43 +0100

    uwu

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ class Info(commands.Cog):
     async def ping(self, ctx):
         """Play table tennis with the bot."""
         message = await ctx.send("Pinging...")
-        latency = round(int(self.bot.latency())) / 1000000
+        latency = self.bot.latency / 1000000
         await message.delete()
         await ctx.send("Ping: **{0}ms**".format(latency))
 
