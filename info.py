e7b28fd85297d52c74b0c877727ec5c8aa79cc4d
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-06 12:43:41 +0100

    rounder beast

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ class Info(commands.Cog):
     async def ping(self, ctx):
         """Play table tennis with the bot."""
         message = await ctx.send("Pinging...")
-        latency = self.bot.latency * 1000
+        latency = round(self.bot.latency) * 1000
         await message.delete()
         await ctx.send("Ping: **{0}ms**".format(latency))
 
