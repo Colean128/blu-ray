a107acb45be81bf78e5a9cd3807757f58276384d
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-07 12:09:51 +0100

    Replace bank cog with economy cog.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ try:
 
 
     bank = {}
-    cogs = ['bank','casino','fun','info','nsfw','owner','settings','tags','economy']
+    cogs = ['casino','fun','info','nsfw','owner','settings','tags','economy']
 
     bot = commands.Bot(command_prefix=pogfix)
 
