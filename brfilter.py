f8a50d098da00791569995703b166ae9b81fb6be
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-07 15:26:51 +0100

    @Everyone @ here

--- brfilter.py
+++ brfilter.py
@@ -1,3 +1,3 @@
 badwords = ['fuck', 'shit', 'dick', 'asshole', 'bitch', 'bastard', 'cunt', 'bugger', 'choad', 'shag', 'wanker', 'wank', 'twat', 'slut', 'whore', 'fag', 'faggot', 'cum', 'retard', 'blowjob', 'handjob', 'bhenchod', 'gandu', 'chutiya', 'saley', 'lund pe charo', 'sex', 'vagina', 'penis', 'pussy', 'tits', 'breasts', 'boobs', 'crap', 'porn', 'slag', 'spag', 'spastic', 'bollocks', 'motherfucker', 'arse', 'minger', 'bullshit', 'feck', 'fanny', 'cock', 'dick', 'cunt']
 
-superbadwords = ['nigger', 'niger', 'nigga', 'niga', 'n word']
+superbadwords = ['@everyone', '@here', 'nigger', 'niger', 'nigga', 'niga', 'n word']
