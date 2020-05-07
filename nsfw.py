cb4257aa823d07371a3e4d284edef110434d6d88
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-05-07 15:02:05 +0100

    ayy does the actual e621 work

--- nsfw.py
+++ nsfw.py
@@ -9,7 +9,9 @@ import asyncio
 import brfilter
 import main
 from discord.ext import tasks, commands
-
+headers = {
+    'User-Agent': 'Blu-Ray 1.1.2 (Discord bot by Zayne64 on GitHub)'
+    }
 class NSFW(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -21,7 +23,7 @@ class NSFW(commands.Cog):
         Command restricted to NSFW channels."""
         if ctx.message.channel.is_nsfw():
             async with aiohttp.ClientSession() as session:
-                async with session.get('https://r34-json-api.herokuapp.com/posts?tags='+str(arg)) as r:
+                async with session.get('https://r34-json-api.herokuapp.com/posts?tags='+str(arg), headers=headers) as r:
                     if r.status == 200:
                         js = await r.json()
                         rand = random.randint(1,100)
@@ -31,6 +33,27 @@ class NSFW(commands.Cog):
                         else:
                             embed = await main.buildEmbed('Rule34 search results for **'+str(arg)+'**.', js[rand]['file_url'])
                             await ctx.send(embed = embed)
+                    else:
+                        await ctx.send('Search failed.')
+        else:
+            await ctx.send("This command is restricted to NSFW channels.")
+
+    @commands.command(pass_context=True)
+    async def e621(self, ctx, arg):
+        """Search e621.
+        Command restricted to NSFW channels."""
+        if ctx.message.channel.is_nsfw():
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://e621.net/post/index.json?limit=100&tags='+str(arg)) as r:
+                    if r.status == 200:
+                        js = await r.json()
+                        rand = random.randint(1,100)
+                        if '.webm' in js[rand]['file_url']:
+                            await ctx.send('e621 search results for **'+str(arg)+'**.')
+                            await ctx.send('Video: '+js[rand]['file_url'])
+                        else:
+                            embed = await main.buildEmbed('e621 search results for **'+str(arg)+'**.', js[rand]['file_url'])
+                            await ctx.send(js)
                     else:
                         await ctx.send('Search failed.')
         else:
