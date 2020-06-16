887fdda8209016db0e1f566988dc4d299be90307
Apfel <32610623+Apfel@users.noreply.github.com>
2020-06-16 22:02:35 +0200

    add about command, add guild filter and update deps

--- Bot/Managers/Events.cs
+++ Bot/Managers/Events.cs
@@ -17,8 +17,10 @@
 
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Bot.Managers
@@ -43,10 +45,38 @@ namespace Bot.Managers
             return Task.CompletedTask;
         }
 
-        public static Task OnGuildJoin(GuildCreateEventArgs e)
+        public static async Task OnGuildJoin(GuildCreateEventArgs e)
         {
+            IReadOnlyCollection<DiscordMember> members = await e.Guild.GetAllMembersAsync();
+
+            int botAmount = 0;
+            DiscordMember currentMember = null;
+            foreach (DiscordMember member in members)
+            {
+                if (member.IsCurrent) currentMember = member;
+                if (member.IsBot) botAmount++;
+            }
+
+            if (currentMember == null) await e.Guild.LeaveAsync();
+
+            int percentage = botAmount / members.Count * 100;
+            if (members.Count > 5 && percentage >= 20)
+            {
+                e.Client.DebugLogger.LogMessage(LogLevel.Info, "Guilds", $"Joined guild \"{e.Guild.Name}\" (ID: {e.Guild.Id}). High bot density: {percentage}% ({members.Count} - {botAmount}).", DateTime.Now);
+
+                IReadOnlyList<DiscordChannel> channels = await e.Guild.GetChannelsAsync();
+                
+                for (int i = 0; i != members.Count + 1; i++) if (channels[i].PermissionsFor(currentMember).HasPermission(Permissions.SendMessages))
+                {
+                    await channels[i].SendMessageAsync(embed: new DiscordEmbedBuilder().WithTitle("High bot density").WithDescription($"Hello everyone!\nI'm sorry to remind you that I'll have to leave this guild.\n{percentage}% of all members in this server are bots, which is more than the allowed amount  of bots that servers can have, if they want to use Blu-Ray.\nDue to limited resources, we cannot, and, even if this wouldn't be a problem, we will not support the idea of bot farms.\n\nIf you still want to use Blu-Ray, remove other bots, please.\n\nIf you believe this judgement is invalid, you can leave an issue on the GitHub repository: https://github.com/Zayne64/blu-ray\n\nSincerely,\nthe team behind the Blu-Ray bot.").Build());
+                    break;
+                }
+
+                await e.Guild.LeaveAsync();
+                return;
+            }
+
             e.Client.DebugLogger.LogMessage(LogLevel.Info, "Guilds", $"Joined guild \"{e.Guild.Name}\" (ID: {e.Guild.Id}).", DateTime.Now);
-            return Task.CompletedTask;
         }
 
         public static Task OnGuildLeave(GuildDeleteEventArgs e)
@@ -61,12 +91,13 @@ namespace Bot.Managers
             return Task.CompletedTask;
         }
 
-        public static Task OnCommandError(CommandErrorEventArgs e)
+        public static async Task OnCommandError(CommandErrorEventArgs e)
         {
-            if (e.Command == null) return Task.CompletedTask;
+            if (e.Command == null) return;
 
             e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "Commands", $"The command \"{e.Command.Name}\" executed by the user {e.Context.User.Username}#{e.Context.User.Discriminator} (ID: {e.Context.User.Id}) in channel \"{e.Context.Channel.Id}\" encountered an error.", DateTime.Now, e.Exception);
-            return Task.CompletedTask;
+            
+            await e.Context.RespondAsync("An error occurred.\nIf this persists, use the `about` command and leave an issue on the GitHub repository.\n");
         }
     }
 }
