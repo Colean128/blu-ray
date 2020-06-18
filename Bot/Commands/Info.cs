9847aef36550848f0808d85789add011b7d3e54b
Marko <32610623+Apfel@users.noreply.github.com>
2020-06-18 15:14:09 +0200

    Implement yet another game command fix (#45)

--- Bot/Commands/Info.cs
+++ Bot/Commands/Info.cs
@@ -63,9 +63,9 @@ namespace Bot.Commands
 
             if (member.Presence.Activity.RichPresence.StartTimestamp != null)
             {
-                TimeSpan span = (TimeSpan)(DateTime.Now.ToUniversalTime() - member.Presence.Activity.RichPresence.StartTimestamp);
                 message += $" for ";
-
+                
+                TimeSpan span = (TimeSpan)(DateTime.Now - member.Presence.Activity.RichPresence.StartTimestamp);
                 if (span.Minutes == 0) message += $"__{span.Seconds} second{(span.Seconds == 1 ? "" : "s")}__";
                 else
                 {
