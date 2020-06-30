fc1c16928bdd374cdb48e60266de6947519e1f3d
Apfel <32610623+Apfel@users.noreply.github.com>
2020-06-30 15:46:24 +0200

    :-)

---
+++ Managers/Database.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Threading.Tasks;
+
+namespace Bot.Managers
+{
+    public class Database
+    {
+        private static SqliteConnection sqlite;
+
+        public static async Task ConnectAsync()
+        {
+            sqlite = new SqliteConnection("Data Source=blu-ray.db");
+            await sqlite.OpenAsync();
+        }
+
+        public static void Disconnect()
+        {
+            sqlite.Close();
+            sqlite.Dispose();
+        }
+    }
+}
