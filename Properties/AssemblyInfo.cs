1591d22861ce871cae82fe4fedbe15baadc9ee75
Marko <32610623+Apfel@users.noreply.github.com>
2020-07-07 00:38:51 +0200

    Bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ae57a89c-8d6e-4f8f-a7df-fc754dc2b320")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1")]
