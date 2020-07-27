ff33d8d23fe0842f55cd9f813ec04b8118544d89
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-07-27 22:51:51 +0100

    Still broken

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,53 @@
+use std::fs::{self, File};
+use std::io::BufReader;
+
 use serenity::client::Client;
-use serenity::model::channel::Message;
-use serenity::prelude::{EventHandler, Context};
 use serenity::framework::standard::{
-    StandardFramework,
-    CommandResult,
-    macros::{
-        command,
-        group
-    }
+	CommandResult,
+	macros::{
+		command,
+		group
+	},
+	StandardFramework
 };
+use serenity::model::channel::Message;
+use serenity::prelude::{Context, EventHandler};
+
+use serde::{Result, Value};
 
 #[group]
 #[commands(ping)]
 struct General;
 
-use std::fs;
-
 struct Handler;
+
+struct Config {
+	token: String
+}
 
 impl EventHandler for Handler {}
 
 fn main() {
-    let config = fs::read_to_string("config.json").unwrap();
-    let v: Value = serde_json::from_str(config);
-    let clienttoken = v["token"];
-    let mut client = Client::new(clienttoken.expect("token"), Handler)
-        .expect("Error creating client");
-    client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("tbr/"))
-        .group(&GENERAL_GROUP));
+	struct ConfigIni { token: String }
+	let path = matches.value_of("config-ini").unwrap();
+	let canonicalized = std::fs::canonicalize(path).context("couldn't resolve ini path")?;
+	let file = File::open(canonicalized).context("couldn't open Config .ini")?;
+	let deserialized: DigitalOceanIni = serde_ini::from_read(file).context("couldn't parse .ini")?;
 
-    if let Err(why) = client.start() {
-        println!("An error occurred while running the client: {:?}", why);
-    }
+	let mut client = Client::new(deserialized.token, Handler)
+		.expect("Error creating client");
+	client.with_framework(StandardFramework::new()
+		.configure(|c| c.prefix("tbr/"))
+		.group(&GENERAL_GROUP));
+
+	if let Err(why) = client.start() {
+		println!("An error occurred while running the client: {:?}", why);
+	}
 }
 
 #[command]
 fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!")?;
+	msg.reply(ctx, "Pong!")?;
 
-    Ok(())
+	Ok(())
 }
